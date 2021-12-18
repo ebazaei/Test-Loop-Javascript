ff63c533147127b5f4c467245f0411c156cb27b9
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-18 09:53:49 +0100

    edit 2

--- js.js
+++ js.js
@@ -1,42 +1,84 @@
-// for(let i=0; i <= 6 ; i++){
-    // if(i==0){
-    //     console.log('Sunday');
-    // }else if(i==1){
-    //     console.log('Monday');
-    // }else if(i==2){
-    //     console.log('Tuesday');
-    // }else if(i==3){
-    //     console.log('Wednesday');
-    // }else if(i==4){
-    //     console.log('Thursday');
-    // }else if(i==5){
-    //     console.log('Friday');
-    // }else if(i==6){
-    //     console.log('Saturday');
-    // }
-// }
+// Loop - for
+console.log('.......::: Loop: for :::.......')
 
-
-let dag = 0;
-
-while (dag <= 6) {
-    if(dag==0){
+for(let i=0; i <= 7 ; i++){
+    if(i==0){
         console.log('Sunday');
-    }else if(dag==1){
+    }else if(i==1){
         console.log('Monday');
-    }else if(dag==2){
+    }else if(i==2){
         console.log('Tuesday');
-    }else if(dag==3){
+    }else if(i==3){
         console.log('Wednesday');
-    }else if(dag==4){
+    }else if(i==4){
         console.log('Thursday');
-    }else if(dag==5){
+    }else if(i==5){
         console.log('Friday');
-    }else if(dag==6){
+    }else if(i==6){
         console.log('Saturday');
-    }else{
-        console.log('edameh')
+    }else{  
+        console.log('---------------------------');
+        console.log('Nexy Week');
+        console.log('');
     }
-    dag++;
 }
 
+
+
+// Loop - While
+console.log('.......::: Loop: While :::.......')
+
+
+let dag2 = 0;
+
+while (dag2 <= 7) {
+    if(dag2==0){
+        console.log('Sunday');
+    }else if(dag2==1){
+        console.log('Monday');
+    }else if(dag2==2){
+        console.log('Tuesday');
+    }else if(dag2==3){
+        console.log('Wednesday');
+    }else if(dag2==4){
+        console.log('Thursday');
+    }else if(dag2==5){
+        console.log('Friday');
+    }else if(dag2==6){
+        console.log('Saturday');
+    }else{  
+        console.log('---------------------------');
+        console.log('Nexy Week');
+        console.log('');
+    }
+    dag2++;
+}
+
+
+// Loop - Do While
+console.log('.......::: Loop: Do While :::.......')
+
+
+let dag3 = 0;
+do{
+    if(dag3==0){
+        console.log('Sunday');
+    }else if(dag3==1){
+        console.log('Monday');
+    }else if(dag3==2){
+        console.log('Tuesday');
+    }else if(dag3==3){
+        console.log('Wednesday');
+    }else if(dag3==4){
+        console.log('Thursday');
+    }else if(dag3==5){
+        console.log('Friday');
+    }else if(dag3==6){
+        console.log('Saturday');
+    }else{      
+        console.log('---------------------------');
+        console.log('Nexy Week');
+    }
+    dag3++;
+}
+while(dag3 < 8)
