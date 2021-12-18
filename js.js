f23427bd8ffc17f3548727df0965072de5fb0fe2
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-18 09:16:57 +0100

    taghir-1

---
+++ js.js
@@ -0,0 +1,42 @@
+// for(let i=0; i <= 6 ; i++){
+    // if(i==0){
+    //     console.log('Sunday');
+    // }else if(i==1){
+    //     console.log('Monday');
+    // }else if(i==2){
+    //     console.log('Tuesday');
+    // }else if(i==3){
+    //     console.log('Wednesday');
+    // }else if(i==4){
+    //     console.log('Thursday');
+    // }else if(i==5){
+    //     console.log('Friday');
+    // }else if(i==6){
+    //     console.log('Saturday');
+    // }
+// }
+
+
+let dag = 0;
+
+while (dag <= 6) {
+    if(dag==0){
+        console.log('Sunday');
+    }else if(dag==1){
+        console.log('Monday');
+    }else if(dag==2){
+        console.log('Tuesday');
+    }else if(dag==3){
+        console.log('Wednesday');
+    }else if(dag==4){
+        console.log('Thursday');
+    }else if(dag==5){
+        console.log('Friday');
+    }else if(dag==6){
+        console.log('Saturday');
+    }else{
+        console.log('edameh')
+    }
+    dag++;
+}
+
